d4d996df8b3d8a99b313e0326832ce215c0f63ca
LUÍS ANDRÉ CASTRO LEMOS <al78644@alunos.utad.pt>
2024-03-28 22:33:49 +0000

    Criado Pastas e Projeto WPF

---
+++ UTAD.ToDo.It-App/UTAD.ToDo.It-App/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace UTAD.ToDo.It_App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
