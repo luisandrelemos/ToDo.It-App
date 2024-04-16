908e768a5fe64c6206b808d268886b218abba91a
Luís Lemos <122064433+luisandrelemos@users.noreply.github.com>
2024-04-16 18:08:07 +0100

    Added Login/Register Page & Initial Page (WIP)

---
+++ UTAD.ToDo.It-App/Página Autenticação/Login.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Data.SqlClient;
+using System;
+using Página_Inicial;
+
+namespace Login_Page
+{
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(password.Password) && password.Password.Length > 0)
+                textPassword.Visibility = Visibility.Collapsed;
+            else
+                textPassword.Visibility = Visibility.Visible;
+        }
+
+        private void textPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            password.Focus();
+        }
+
+        private void Login_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(@"Server=LEMOS-TUF;Database=LoginRegisterDB;Integrated Security=True;");
+                con.Open();
+                string add_data = "SELECT * FROM [dbo].[userData] where email=@email and password=@password";
+                SqlCommand cmd = new SqlCommand(add_data, con);
+
+                cmd.Parameters.AddWithValue("@email", email.Text);
+                cmd.Parameters.AddWithValue("@password", password.Password);
+                cmd.ExecuteNonQuery();
+                int Count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                con.Close();
+
+                email.Text = "";
+                password.Password = "";
+                if (Count > 0)
+                {
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("O E-Mail ou a Password estão incorretos!");
+                }
+
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void txtEmail_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(email.Text) && email.Text.Length > 0)
+                textEmail.Visibility = Visibility.Collapsed;
+            else
+                textEmail.Visibility = Visibility.Visible;
+        }
+
+        private void textEmail_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            email.Focus();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Registar r1 = new Registar();
+            this.Close();
+            r1.Show();
+        }
+    }
+}
---
+++ UTAD.ToDo.It-App/Página Autenticação/Registar.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlTypes;
+using System.Text.RegularExpressions;
+
+namespace Login_Page
+{
+    public partial class Registar : Window
+    {
+        public Registar()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(password.Password) && password.Password.Length > 0)
+                textPassword.Visibility = Visibility.Collapsed;
+            else
+                textPassword.Visibility = Visibility.Visible;
+        }
+
+        private void textPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            password.Focus();
+        }
+
+        private void Registar_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(@"Server=LEMOS-TUF;Database=LoginRegisterDB;Integrated Security=True;");
+                con.Open();
+
+                if (string.IsNullOrEmpty(username.Text) || string.IsNullOrEmpty(password.Password) || string.IsNullOrEmpty(email.Text))
+                {
+                    throw new Exception("Introduza os campos!");
+                }
+
+                if (!IsValidEmail(email.Text))
+                {
+                    throw new Exception("O E-Mail digitado não é válido!");
+                }
+
+                // Verifica se o e-mail já existe no banco de dados
+                string checkEmailQuery = "SELECT COUNT(*) FROM [dbo].[userData] WHERE email = @email";
+                SqlCommand checkEmailCmd = new SqlCommand(checkEmailQuery, con);
+                checkEmailCmd.Parameters.AddWithValue("@email", email.Text);
+                int emailCount = (int)checkEmailCmd.ExecuteScalar();
+
+                if (emailCount > 0)
+                {
+                    throw new Exception("O E-Mail já está em uso!");
+                }
+
+                string add_data = "INSERT INTO [dbo].[userData] VALUES(@email, @username, @password)";
+                SqlCommand cmd = new SqlCommand(add_data, con);
+
+                cmd.Parameters.AddWithValue("@email", email.Text);
+                cmd.Parameters.AddWithValue("@username", username.Text);
+                cmd.Parameters.AddWithValue("@password", password.Password);
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                email.Text = "";
+                username.Text = "";
+                password.Password = "";
+
+                Login l1 = new Login();
+                this.Close();
+                l1.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void txtEmail_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(email.Text) && email.Text.Length > 0)
+                textEmail.Visibility = Visibility.Collapsed;
+            else
+                textEmail.Visibility = Visibility.Visible;
+        }
+
+        private void textEmail_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            email.Focus();
+        }
+
+        private void txtUsername_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(username.Text) && username.Text.Length > 0)
+                textUsername.Visibility = Visibility.Collapsed;
+            else
+                textUsername.Visibility = Visibility.Visible;
+        }
+
+        private void textUsername_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            username.Focus();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Login l1 = new Login();
+            this.Close();
+            l1.Show();
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            // Expressão regular para verificar a validade do e-mail
+            string pattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
+            Regex regex = new Regex(pattern);
+            return regex.IsMatch(email);
+        }
+    }
+}
