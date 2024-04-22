3fa8f28ed0866d6ef49c9b6c24340e7d2eb59e60
Luís Lemos <122064433+luisandrelemos@users.noreply.github.com>
2024-04-22 01:04:52 +0100

    Working in Criar Tarefa Page

--- UTAD.ToDo.It-App/Aplicação ToDo.IT/Página Inicial/PáginaNovaTarefa.xaml.cs
+++ UTAD.ToDo.It-App/Aplicação ToDo.IT/Página Inicial/PáginaNovaTarefa.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Aplicação_ToDo.IT.Página_Inicial
 {
@@ -22,9 +13,47 @@ namespace Aplicação_ToDo.IT.Página_Inicial
             InitializeComponent();
         }
 
-        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void txtAdd_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(add.Text) && add.Text.Length > 0)
+                textAdd.Visibility = Visibility.Collapsed;
+            else
+                textAdd.Visibility = Visibility.Visible;
+        }
+
+        private void textAdd_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            add.Focus();
+        }
+
+        private void PriorityComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void RepeatComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void tb_mensagem_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void GuardarNovaTarefa_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LembreteTarefa_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
