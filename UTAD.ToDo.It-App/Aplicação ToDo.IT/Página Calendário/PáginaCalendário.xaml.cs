0a7bb45cd22bd7a14fd02a78cab8a990e6f51f1d
Carolpm28 <pinheirocarolina44@gmail.com>
2024-04-24 17:00:54 +0100

    atualização do calendário

--- UTAD.ToDo.It-App/Aplicação ToDo.IT/Página Calendário/PáginaCalendário.xaml.cs
+++ UTAD.ToDo.It-App/Aplicação ToDo.IT/Página Calendário/PáginaCalendário.xaml.cs
@@ -4,6 +4,8 @@ using Aplicação_ToDo.IT.Página_Inicial;
 using Aplicação_ToDo.IT.Página_Personalizar;
 using Aplicação_ToDo.IT.Página_Tarefas;
 using Aplicação_ToDo.IT.SaveData;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows;
 
 namespace Aplicação_ToDo.IT.Página_Calendário
@@ -11,6 +13,7 @@ namespace Aplicação_ToDo.IT.Página_Calendário
 
     public partial class PáginaCalendário : Window
     {
+        
         public PáginaCalendário()
         {
             InitializeComponent();
@@ -66,5 +69,97 @@ namespace Aplicação_ToDo.IT.Página_Calendário
         {
             Application.Current.Shutdown();
         }
+
+        //-----------------------Create an event Data Model----------------------------
+
+        public class Meeting : INotifyPropertyChanged
+        {
+            DateTime from, to;
+            string eventName;
+            bool isAllDay;
+            string startTimeZone, endTimeZone;
+            //Brush color;
+            public Meeting()
+            {
+            }
+
+            public DateTime From
+            {
+                get { return from; }
+                set
+                {
+                    from = value;
+                    RaisePropertyChanged("From");
+                }
+            }
+
+            public DateTime To
+            {
+                get { return to; }
+                set
+                {
+                    to = value;
+                    RaisePropertyChanged("To");
+                }
+            }
+
+            public bool IsAllDay
+            {
+                get { return isAllDay; }
+                set
+                {
+                    isAllDay = value;
+                    RaisePropertyChanged("IsAllDay");
+                }
+            }
+            public string EventName
+            {
+                get { return eventName; }
+                set
+                {
+                    eventName = value;
+                    RaisePropertyChanged("EventName");
+                }
+            }
+            public string StartTimeZone
+            {
+                get { return startTimeZone; }
+                set
+                {
+                    startTimeZone = value;
+                    RaisePropertyChanged("StartTimeZone");
+                }
+            }
+            public string EndTimeZone
+            {
+                get { return endTimeZone; }
+                set
+                {
+                    endTimeZone = value;
+                    RaisePropertyChanged("EndTimeZone");
+                }
+            }
+
+            /*public Brush Color
+            {
+                get { return color; }
+                set
+                {
+                    color = value;
+                    RaisePropertyChanged("Color");
+                }
+            }*/
+
+            public event PropertyChangedEventHandler PropertyChanged;
+            protected virtual void RaisePropertyChanged(string propertyName, object oldValue = null)
+            {
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+        }
+        //-----------------------------------------------------------------------------------//
+
+        
     }
 }
+
