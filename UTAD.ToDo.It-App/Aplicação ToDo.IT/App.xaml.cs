4e3ab5f88f08fa96fe464cddf1551f8c02d49d4a
LUÍS ANDRÉ CASTRO LEMOS <al78644@alunos.utad.pt>
2024-04-24 15:48:56 +0100

    Validate the License Key

--- UTAD.ToDo.It-App/Aplicação ToDo.IT/App.xaml.cs
+++ UTAD.ToDo.It-App/Aplicação ToDo.IT/App.xaml.cs
@@ -1,18 +1,17 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Syncfusion.Licensing;
 
 namespace Aplicação_ToDo.IT
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class ToDoItApp : Application
+
+    public partial class App : Application
     {
-        public ToDoItApp()
+        public App()
         {
             //Register Syncfusion license
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NBaF5cWWNCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWXtfeXRdRmJfUEZyVko=");
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzI0MDk4NkAzMjM1MmUzMDJlMzBIZURTL25TUW8vaFRRVVc4V1pRSGVyVWFBYk1pK3g1NWFQc2w2Qng1WUF3PQ==");
         }
     }
 }
